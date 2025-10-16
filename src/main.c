60e13b41c49ebadb8beb28b4a5be79dc6b954842
Gustavo Fernandes <fgustavo.santoss@gmail.com>
2025-10-16 15:21:20 -0300

    botao com interrupcao

--- src/main.c
+++ src/main.c
@@ -5,27 +5,30 @@
 
 // --- Configuração de LEDs via DeviceTree ---
 #define LED_A_NODE DT_ALIAS(led0)  // LED verde
+#define LED_C_NODE DT_ALIAS(led1)  // LED azul
 #define LED_B_NODE DT_ALIAS(led2)  // LED vermelho
+
+#define BUTTON_NODE DT_NODELABEL(user_button_0)
 
 static const struct gpio_dt_spec ledA = GPIO_DT_SPEC_GET(LED_A_NODE, gpios);
 static const struct gpio_dt_spec ledB = GPIO_DT_SPEC_GET(LED_B_NODE, gpios);
+static const struct gpio_dt_spec ledc = GPIO_DT_SPEC_GET(LED_C_NODE, gpios);
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON_NODE, gpios);
+static struct gpio_callback button_cb_data;
 
 // --- Prioridades e tempos ---
-#define PRIO_THREAD_A 7   // Maior prioridade (número menor)
-#define PRIO_THREAD_B 5   // Menor prioridade
-#define TEMPO_A_MS   900   // Thread A dorme
-#define TEMPO_B_MS   1250   // Thread B dorme
+#define PRIO_THREAD_A 5   // Maior prioridade (número menor)
+#define PRIO_THREAD_B 7   // Menor prioridade
+#define TEMPO_A_MS   1500   // Thread A dorme
+#define TEMPO_B_MS   1000   // Thread B dorme
 
 // ----------------------------------------------------
 // THREAD A — Tarefa curta, alta prioridade
 // ----------------------------------------------------
 void thread_A(void *p1, void *p2, void *p3)
 {
-    uint64_t start_ms, end_ms;
-
     while (1) {
-
-        start_ms = k_uptime_get();  // tempo atual em ms
 
         gpio_pin_set_dt(&ledA, 1);  // Liga LED verde
 
@@ -33,14 +36,8 @@ void thread_A(void *p1, void *p2, void *p3)
         for (volatile int i = 0; i < 200000; i++) {}
 
         gpio_pin_set_dt(&ledA, 0);  // Desliga LED verde
-
-        end_ms = k_uptime_get();
-
-        printk("Tempo inicio A: %llu ms\n", start_ms);
-        printk("Tempo final A: %llu ms\n", end_ms);    
-
+   
         k_msleep(TEMPO_A_MS);       // Dorme — libera CPU
-        
 
 
     }
@@ -51,10 +48,7 @@ void thread_A(void *p1, void *p2, void *p3)
 // ----------------------------------------------------
 void thread_B(void *p1, void *p2, void *p3)
 {
-    uint64_t start_ms, end_ms;
-
     while (1) {
-        start_ms = k_uptime_get();
         gpio_pin_set_dt(&ledB, 1);  // Liga LED vermelho
 
         // Simula processamento mais longo 
@@ -65,24 +59,21 @@ void thread_B(void *p1, void *p2, void *p3)
         }
 
         gpio_pin_set_dt(&ledB, 0);  // Desliga LED vermelho
-
-        end_ms = k_uptime_get();
-
-        printk("Tempo inicio B: %llu ms\n", start_ms);
-        printk("Tempo final B: %llu ms\n", end_ms); 
-
         k_msleep(TEMPO_B_MS);       // Dorme um pouco
-
     }
 }
 
 // ----------------------------------------------------
 // Definição das threads
 // ----------------------------------------------------
-K_THREAD_DEFINE(a_tid, 512, thread_A, NULL, NULL, NULL,
-                PRIO_THREAD_A, 0, 0);
-K_THREAD_DEFINE(b_tid, 512, thread_B, NULL, NULL, NULL,
-                PRIO_THREAD_B, 0, 0);
+K_THREAD_DEFINE(a_tid, 512, thread_A, NULL, NULL, NULL, PRIO_THREAD_A, 0, 0);
+K_THREAD_DEFINE(b_tid, 512, thread_B, NULL, NULL, NULL, PRIO_THREAD_B, 0, 0);
+
+// ISR - Toggle LED
+void button_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+    gpio_pin_toggle_dt(&ledc);
+}
 
 // ----------------------------------------------------
 // Função principal
@@ -95,6 +86,15 @@ void main(void)
     }
     gpio_pin_configure_dt(&ledA, GPIO_OUTPUT_INACTIVE);
     gpio_pin_configure_dt(&ledB, GPIO_OUTPUT_INACTIVE);
+
+    // Configurar LED e botão com pull-up
+    gpio_pin_configure_dt(&ledc, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&button, GPIO_INPUT | GPIO_PULL_UP);
+    
+    // Configurar interrupção na borda de descida
+    gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_FALLING);
+    gpio_init_callback(&button_cb_data, button_isr, BIT(button.pin));
+    gpio_add_callback(button.port, &button_cb_data);
 
     printk("=== Demonstração de Preempção com LEDs ===\n");
     printk("Thread A (vermelho): prioridade %d\n", PRIO_THREAD_A);
